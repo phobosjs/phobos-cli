8195b9653a2bc385c92e544ff364dc8424f9f454
mtimofiiv <github@fiiv.io>
2016-09-08 17:37:53 +0200

    removing the .git folder from the clone

--- tasks/new.js
+++ tasks/new.js
@@ -12,23 +12,34 @@ module.exports = function(_) {
     action: (name) => {
       _.info(`=> Initializing project with name '${name}'...`);
 
-      const command = `git clone ${STARTER_REPO} ${_.directory}/${name}`;
+      const clone = `git clone ${STARTER_REPO} ${_.directory}/${name}`;
 
-      const execute = _.shell(command, (err, stdout, stderr) => {
+      const command = _.shell(clone, (err, stdout, stderr) => {
         if (err) {
           _.error(err);
           _.info(`=> Unable to complete command due to error`);
         }
       });
 
-      execute.on('exit', (code) => {
-        if (code === 0) {
+      command.on('exit', code => {
+        if (code !== 0) return _.info(`=> Unable to complete command due to error`);
+
+        const deleteGitFolder = `rm -rf ${_.directory}/${name}/.git`;
+
+        const command = _.shell(deleteGitFolder, (err, stdout, stderr) => {
+          if (err) {
+            _.error(err);
+            _.info(`=> Unable to complete command due to error`);
+          }
+        });
+
+        command.on('exit', code => {
+          if (code !== 0) return _.info(`=> Unable to complete command due to error`);
+
           const pjsonPath = `${_.directory}/${name}/package.json`;
           const pjson = require(pjsonPath);
 
           pjson.name = name;
-          pjson.author = '';
-          pjson.description = 'A brand spanking new Phobos.js project';
 
           fs.writeFile(pjsonPath, JSON.stringify(pjson, null, 2), (err) => {
             if (err) {
@@ -39,9 +50,7 @@ module.exports = function(_) {
 
             _.info(`=> All done - navigate over to the ${name} folder to get started`);
           });
-        } else {
-          _.info(`=> Unable to complete command due to error`);
-        }
+        });
       });
     }
 
